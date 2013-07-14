e5a972962d3ad4c10b54ab785a8bfb7e326d891a
Rami Ahmed Bock <Rami_Ahmed00@hotmail.com>
2013-07-14 20:16:24 +0200

    First commit to DFA v2
    - Stripped all "unnecessary" files (according to
    https://build.phonegap.com/docs/app-building)

---
+++ www/js/formhandler.js
@@ -0,0 +1,85 @@
+/* Script written by Rami@alphastagestudios.com
+    All Copyrights preserved.
+*/
+
+function initContactFormHandler() {
+    $('#kontakt-send').on('click', function(evt) {
+    evt.preventDefault();
+    $.post('http://danmarksflyttemandapp.herokuapp.com/kontaktform-handler.php', {
+        Company: $('#company_input').val(),
+        Name: $('#name_input').val(),
+        Address: $('#address_input').val(),
+        City: $('#zipcode_input').val(),
+        Email: $('#email_input').val(),
+        Telephone: $('#telephone_input').val(),
+        Subject: $('#subject_input').val(),
+        Message: $('#message_input').val()
+
+        // HTML function
+        }, function(html) {
+            // Place HTML in string
+            var response = html;
+
+            // PHP was done and email sent
+            if (response == "success") {
+                $('#kontaktform').html('<h2>Din kontakt besked er sendt!</h2><p class="orange">Tak for din besked! Vi vender tilbage til dig snarest.</p>');
+            }
+            else {
+                navigator.notification.alert('Fejl: ' + response);
+                return false;
+            }
+        });
+    });
+};
+
+function initTilbudFormHandler() {
+    $('#tilbud-send').on('click', function(evt) {
+        evt.preventDefault();
+        $.post('http://danmarksflyttemandapp.herokuapp.com/tilbudform-handler.php', {
+            Name: $('#tilbud-name_input').val(),
+            Telephone: $('#tilbud-telephone_input').val(),
+            Email: $('#tilbud-email_input').val(),
+
+            FromAddress: $('#from-address_input').val(),
+            FromZip: $('#from-zipcode_input').val(),
+            FromCity: $('#from-city_input').val(),
+            FromRegion: $('#from-region_input').val(),
+            FromElevator: $('#from-elevator_input').val(),
+            FromParking: $('#from-parking_input').val(),
+            FromSize: $('#from-areal_input').val(),
+            FromRooms: $('#from-rooms_input').val(),
+
+            ToAddress: $('#to-address_input').val(),
+            ToZip: $('#to-zipcode_input').val(),
+            ToCity: $('#to-city_input').val(),
+            ToRegion: $('#to-region_input').val(),
+            ToElevator: $('#to-elevator_input').val(),
+            ToParking: $('#to-parking_input').val(),
+            ToSize: $('#to-areal_input').val(),
+            ToRooms: $('#to-rooms_input').val(),
+
+            MoveDate: $('#moving-date_input').val(),
+            Flexible: $('#flexible-date_input').val(),
+            AmountMen: $('#amount-men_input').val(),
+            Packing: $('#pack-up_input').val(),
+            Description: $('#description_input').val()
+
+        }, function(html) {
+            var response = html;
+
+            if (response == "success") {
+                $('#indhenttilbud').html('<h2>Din besked er sendt!</h2><p class="orange">Tak for din besked! Du vil modtage et skriftligt svar med et uforpligtende tilbud indenfor 24 timer.</p>');
+            }
+            else {
+                navigator.notification.alert('Fejl: ' + response);
+                return false;
+            }
+        });
+    });
+};
+
+function initFormHandler() {
+    initContactFormHandler();
+    initTilbudFormHandler();
+};
+
---
+++ www/js/init.js
@@ -0,0 +1,197 @@
+/* Script written by Rami@alphastagestudios.com
+    All Copyrights preserved.
+*/
+
+// include touch on desktop browsers only
+if (!((window.DocumentTouch && document instanceof DocumentTouch) || 'ontouchstart' in window)) {
+  var script = document.createElement("script");
+  script.src = "plugins/af.desktopBrowsers.js";
+  var tag = $("head").append(script);
+  $.os.android = true; //let's make it run like an android device
+  $.os.desktop = true;
+};
+
+
+$.ui.autoLaunch = false; //By default, it is set to true and you're app will run right away.  We set it to false to show a splashscreen
+
+$().ready(function() {
+    $.ui.useOSThemes=false;
+    $.ui.blockPageScroll();
+    $.ui.showBackbutton=false;
+    $.ui.openLinksNewTab = false;
+
+    window.setTimeout(function () {
+        if (!fireOnce)
+            $.ui.launch();
+        else
+            delete fireOnce;
+    }, 1000);
+});
+
+function openPDF() {
+    var url = 'http://www.danmarksflyttemand.dk/forsikring/documents/Danmarksflyttemandvilk%C3%A5rogbetingelser.pdf',
+        googleUrl = 'http://docs.google.com/viewer?url=';
+    window.open(
+        googleUrl + url,
+        '_blank',
+        'location=no');
+};
+
+function loadPage(page, insertIntoId) {
+    var req = new XMLHttpRequest();
+    req.open("GET", page, false);
+    req.send(null);
+    var page = req.responseText;
+    $(insertIntoId).html(page);
+};
+
+function initIndexPage() {
+    $.ui.toggleHeaderMenu(false);
+    $.ui.toggleNavMenu(false);
+
+    $('#index').one('unloadpanel', function() {
+        $.ui.toggleHeaderMenu(true);
+        $.ui.toggleNavMenu(true);
+    });
+};
+
+function fixDateOnAndroid() {
+    if (device.platform == "Android") {
+        $('moving-date_input').prop('type', 'number');
+    }
+}
+
+/* BACK BUTTON */
+function onExitConfirm(button) {
+    if (button == 1) {
+        navigator.app.exitApp();
+    }
+}
+
+function onBackKeyDown() {
+    navigator.notification.confirm("Vil du virkeligt forlade Danmarks Flyttemand App?", onExitConfirm, "Forlad App", "Ja,Nej");
+}
+
+function initBackButton() {
+    document.addEventListener("backbutton", onBackKeyDown, false);
+}
+
+/* Google Analytics Page Tracking */
+function initGAanalytics() {
+    $('#afui .panel').on('loadpanel', function() {
+        try {
+            page = location.hash.substring(1);
+            if (page && page.length > 1) {
+            //    console.log("google analytics pageshow url: " + page);
+                ga_storage._trackPageview(page);
+            } else {
+            //    console.log("google analytics pageshow default url");
+                ga_storage._trackPageview('/index');
+            }
+        }
+        catch (e) {
+            navigator.notification.alert('Google Analytics error: ' + e);
+            console.log('error google analytics' + e);
+        }
+    });
+}
+
+/* SWIPING */
+function initSwiping() {
+    $('#index').one('unloadpanel', function(evt) {
+
+        var getPageId = function(index) {
+            var page = '';
+            switch (index) {
+                case 0: page = 'services'; break;
+                case 1: page = 'tilbud'; break;
+                case 2: page = 'kontakt'; break;
+                case 3: page = 'omos'; break;
+            }
+            return page;
+        };
+
+        var getIndex = function(page) {
+            var index = -1;
+            switch (page) {
+                case 'services': index = 0; break;
+                case 'tilbud': index = 1; break;
+                case 'kontakt': index = 2; break;
+                case 'omos': index = 3; break;
+            }
+            return index;
+        };
+
+        $('#afui').on('swipeLeft', function(evt) {
+            var currentPanel = $.ui.activeDiv.id,
+                newPanel;
+            var currentIndex = getIndex(currentPanel);
+
+            if (currentIndex < 3) {
+                newPanel = getPageId(currentIndex+1);
+            }
+            else {
+                newPanel = getPageId(0);
+            }
+
+            $.ui.loadContent('#' + newPanel, false, false, 'slide');
+        });
+        $('#afui').on('swipeRight', function(evt) {
+            var currentPanel = $.ui.activeDiv.id,
+                newPanel;
+            var currentIndex = getIndex(currentPanel);
+
+            if (currentIndex > 0) {
+                newPanel = getPageId(currentIndex-1);
+            }
+            else {
+                newPanel = getPageId(3);
+            }
+            $.ui.loadContent('#' + newPanel, false, true, 'slide');
+        });
+    });
+}
+
+/* AUTO FONT SIZE */
+function initAutoFontSize() {
+    var windowWidth = window.innerWidth;
+
+    var fontFactor = 0.04;
+    var fontSize = windowWidth * fontFactor;
+    $('body,html').css("font-size", '' + fontSize + 'px');
+}
+
+/* INITIALIZATION */
+var fireOnce = false;
+
+//This function will get executed when $.ui.launch has completed
+$.ui.ready(function () {
+    new FastClick(document.body);
+
+    loadPage('tilbudform.html', '#indhenttilbud');
+    loadPage('kontaktform.html', '#kontaktform');
+    loadPage('prisliste.html', '#kasseprisliste');
+
+    $('#betingelserlink').on('click', function(evt) {
+        evt.preventDefault();
+        openPDF();
+    });
+
+    initIndexPage();
+    initMaps();
+    initFormHandler();
+    fixDateOnAndroid();
+    initBackButton();
+    initGAanalytics();
+    initSwiping();
+    initAutoFontSize();
+});
+
+/* This code is used for native apps */
+var onDeviceReady = function () {
+    fireOnce = true;
+    window.setTimeout(function () {
+        $.ui.launch();
+    }, 1000);
+};
+document.addEventListener("deviceready", onDeviceReady, false);
