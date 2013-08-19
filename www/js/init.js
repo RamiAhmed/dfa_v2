d577b9f4ae808e11bfa907989a686d797c58f096
Rami Ahmed Bock <Rami_Ahmed00@hotmail.com>
2013-08-19 16:29:32 +0200

    Added a timeout to auto size

--- www/js/init.js
+++ www/js/init.js
@@ -52,7 +52,7 @@ function onExitConfirm(button) {
 }
 
 function onBackKeyDown() {
-    navigator.notification.confirm("Vil du virkeligt forlade Danmarks Flyttemand?", onExitConfirm, "Exit", "Ja,Nej");
+    navigator.notification.confirm("Vil du forlade Danmarks Flyttemand?", onExitConfirm, "Exit", "Ja,Nej");
 }
 
 function initBackButton() {
@@ -148,7 +148,7 @@ function initAutoFontSize() {
     autoSize();
 
     $(window).on('orientationchange', function() {
-        autoSize();
+        window.setTimeout(autoSize, 1000);
     });
 }
 
