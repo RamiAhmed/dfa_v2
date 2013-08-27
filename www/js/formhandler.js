6b30dea3a35800bedfde1376b7058bfb1debb326
Rami Ahmed Bock <Rami_Ahmed00@hotmail.com>
2013-08-28 00:20:24 +0200

    Fixed error feedback

--- www/js/formhandler.js
+++ www/js/formhandler.js
@@ -19,8 +19,8 @@ function initContactFormHandler() {
             success: function(data) {
                 navigator.notification.alert('Success: ' + data + ', .d: ' + data.d);
             },
-            error: function(data) {
-                navigator.notification.alert('Error: ' + data + ', .d: ' + data.d);
+            error: function(msg, status) {
+                navigator.notification.alert('Error: ' + msg + ', status: ' + status);
             }
         });
 
